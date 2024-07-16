00d3d4b3e500ddc077ce0867ed22728b6c0028c9
jayCloveb <juancamilo.amortegui@gmail.com>
2024-07-16 19:17:54 +0200

    Delete out/build/mypreset directory

--- out/build/mypreset/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/juanc/Documents/GitHub/food_inventory
-# Build directory: C:/Users/juanc/Documents/GitHub/food_inventory/out/build/mypreset
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
